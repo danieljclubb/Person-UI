5752f8927a72ca3b820a5cf2f9cd9dc4cec3e0ad
Daniel Clubb <danieljclubb@gmail.com>
2019-02-15 12:19:20 -0600

    First Commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Clubb, Daniel J. (MU-Student) on 2/15/19.
+//  Copyright © 2019 Clubb, Daniel J. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
